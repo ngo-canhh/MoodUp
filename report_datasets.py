c86132719c308eec6abe12f7b0548337e1ad8194
ngocanhh <ngocanhh@ngocanhhs-MacBook-Air.local>
2024-10-20 22:19:53 +0700

    neural network

--- report_datasets.py
+++ report_datasets.py
@@ -1,22 +1,9 @@
-import kagglehub
-import shutil
-import os
 import pandas as pd
+import os
 from ydata_profiling import ProfileReport
 
 
-
-if not os.path.exists(os.getcwd() + '/datasets'):
-    os.mkdir('datasets')
-
-dataset_urls = ['anthonytherrien/depression-dataset', 'divaniazzahra/mental-health-dataset', 'mdismielhossenabir/psychosocial-dimensions-of-student-life']
-
-for url in dataset_urls:
-    path = kagglehub.dataset_download(url)
-    path = path + '/' + os.listdir(path)[0]
-    shutil.move(path, os.getcwd() + '/datasets/' + os.path.basename(path))
-
-datasets = [pd.read_csv(os.getcwd() + '/datasets/' + file) for file in os.listdir(os.getcwd() + '/datasets')]
+datasets = [pd.read_csv(os.getcwd() + '/datasets/' + file) for file in os.listdir(os.getcwd() + '/datasets') if file.endswith('.csv')]
 
 if not os.path.exists(os.getcwd() + '/reports'):
     os.mkdir(os.getcwd() + '/reports')
