761f7f13c4dd0a6127ca36aad801c3ae25f264dc
ngocanhh <ngocanhh@ngocanhhs-MacBook-Air.local>
2024-10-21 11:59:24 +0700

    K fix dc neural network T.T

--- neural_network/prepare_data.py
+++ neural_network/prepare_data.py
@@ -1,7 +1,12 @@
+# file này có công việc tiền xử lý dữ liệu 
+
+
 import os
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler 
+
 
 # Nếu đã split data rồi thì đừng làm lại nữa
 if (os.path.exists('dataset.npz')):
@@ -18,13 +23,19 @@ unique_labels = y.unique()
 label_to_number = {label: idx for idx, label in enumerate(unique_labels)}
 number_to_label = {v: k for k, v in label_to_number.items()}
 y = y.map(label_to_number).values
+y = y.reshape(-1, 1)
+
+# standard scale
+sc = StandardScaler() 
+x = sc.fit_transform(x) 
 
 # Tách dataset
-x_train, x_, y_train, y_ = train_test_split(x, y, test_size=0.80, random_state=13)
-x_cv, x_test, y_cv, y_test = train_test_split(x_, y_, test_size=0.50, random_state=10)
+x_train, x_, y_train, y_ = train_test_split(x, y, test_size=0.30, random_state=1)
+x_cv, x_test, y_cv, y_test = train_test_split(x_, y_, test_size=0.50, random_state=1)
 
 # Lưu lại dataset
 np.savez('dataset.npz', 
+         x=x, y=y,
          x_train=x_train, y_train=y_train, 
          x_test=x_test, y_test=y_test, 
          x_cv=x_cv, y_cv=y_cv,
--- neural_network/train_model.py
+++ neural_network/train_model.py
@@ -3,17 +3,21 @@ import pandas as pd
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+from tensorflow.keras.callbacks import EarlyStopping
+
 
 # Load lại dataset
 data = np.load('dataset.npz')
-x_train = data['x_train']
-y_train = data['y_train']
+x_train = data['x']
+y_train = data['y']
+print(data['x'].shape)
+print(x_train.shape)
 
 # model
 model = Sequential(
     [
         tf.keras.Input(shape=(14,)),
-        Dense(16, activation= 'relu'),
+        Dense(32, activation= 'relu'),
         Dense(16, activation= 'relu'),
         Dense(4, activation= 'linear'),
     ], name = "nn_model"
@@ -21,10 +25,14 @@ model = Sequential(
 
 model.compile(
     loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = True),
-    optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
 )
-# Train
-model.fit(x_train, y_train, epochs= 40)
+
+# Train, sử dụng early stop
+# early_stopping = EarlyStopping(monitor='loss', patience=3, restore_best_weights=True)
+model.fit(x_train, y_train, epochs=50, batch_size=16)
+
+print(np.mean(y_train == np.argmax(tf.nn.softmax(model.predict(x_train)).numpy(), axis = 1).reshape(-1, 1)))
 
 # Lưu lại dùng để đánh giá
 model.save('nn_model.keras')
